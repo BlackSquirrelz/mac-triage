3ef03775b104c5c6327cdd21a60a2d29cffa2239
Tobias Weisskopf <tobias.weisskopf@pm.me>
2023-02-12 12:38:42 +0100

    Porting 2 SwiftUI

---
+++ MacTriage/MacTriage/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MacTriage
+//
+//  Created by BlackSquirrelz on 12.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacTriage/MacTriage/MacTriageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacTriageApp.swift
+//  MacTriage
+//
+//  Created by BlackSquirrelz on 12.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MacTriageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
